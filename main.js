10aa9b63165b1568a46f85a90d1147a5533ddaa2
Aqua <99206531+WedleySilva@users.noreply.github.com>
2022-03-22 15:17:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function multiplicar(valor1, valor2) {
+    return valor1 * valor2
+}
+function somar(valor1, valor2) {
+    return valor1 + valor2
+}
+
+function dividir(valor1, valor2) {
+    return valor1 / valor2
+}
+
+function subtrair(valor1, valor2) {
+    return valor1 - valor2
+}
+
+function handleBtMult() {
+    const v1 = document.getElementById('valor1').value
+    const v2 = document.getElementById('valor2').value
+    document.getElementById('resultado').innerHTML = multiplicar(v1, v2)
+}
+
+function handleBtSomar () {
+    const v1 = (document.getElementById('valor1').value)
+    const v2 = (document.getElementById('valor2').value)
+    document.getElementById('resultado').innerHTML = somar(v1, v2)
+}
+
+function handleBtSub () {
+    const v1 = (document.getElementById('valor1').value)
+    const v2 = (document.getElementById('valor2').value)
+    document.getElementById('resultado').innerHTML = subtrair(v1, v2)
+}
+
+function handleBtDiv () {
+    const v1 = (document.getElementById('valor1').value)
+    const v2 = (document.getElementById('valor2').value)
+    document.getElementById('resultado').innerHTML = dividir(v1, v2)
+}
+
+document.getElementById('btMult').addEventListener('click',handleBtMult)
+document.getElementById('btSomar').addEventListener('click', handleBtSomar)
+document.getElementById('btDiv').addEventListener('click', handleBtDiv)
+document.getElementById('btSub').addEventListener('click', handleBtSub)
+
